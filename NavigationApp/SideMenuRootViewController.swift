8b97b7d6ae99cd7cf54f2faaf457292ffb1bcf37
Maksim Semeniuk <maxim.semeniuk@antares-software.ru>
2021-06-27 20:07:07 +0300

    Added side menu

---
+++ NavigationApp/SideMenuRootViewController.swift
@@ -0,0 +1,13 @@
+//
+//  SideMenuRootViewController.swift
+//  NavigationApp
+//
+//  Created by Maksim Semeniuk on 27.06.2021.
+//
+
+import UIKit
+
+class SideMenuRootViewController: UIViewController {
+    @IBAction func buttonPressed(_ sender: Any) {
+    }
+}
