b11b58bb9036de64d6c7a0616ed5edef4b2b145e
Maksim Semeniuk <maxim.semeniuk@antares-software.ru>
2021-06-27 16:52:07 +0300

    Fixed constraints

--- NavigationApp/MapViewController.swift
+++ NavigationApp/MapViewController.swift
@@ -12,14 +12,18 @@ class MapViewController: UIViewController {
     
     @IBOutlet weak var mapView: GMSMapView!
     @IBOutlet weak var recordInfoView: UIView!
-    @IBOutlet weak var recordInfiStackView: UIStackView!
+    @IBOutlet weak var recordInfoStackView: UIStackView!
     @IBOutlet weak var distanceLabel: UILabel!
     @IBOutlet weak var timeLabel: UILabel!
     @IBOutlet weak var recordButton: UIButton!
     @IBOutlet weak var saveButton: UIButton!
+    @IBOutlet weak var recordInfoViewBottomConstraint: NSLayoutConstraint!
+    @IBOutlet weak var recordInfoViewHeightConstraint: NSLayoutConstraint!
     
     private let locationManager = CLLocationManager()
+    private let path = GMSMutablePath()
     private var observation: NSKeyValueObservation?
+    private var isRecording = false
     private var firstUserLocation: CLLocation? {
         didSet {
             // Set first user location and move camera to it. When invalidate observation.
@@ -33,16 +37,49 @@ class MapViewController: UIViewController {
         super.viewDidLoad()
         mapView.delegate = self
         locationManager.delegate = self
-        mapView.isMyLocationEnabled = true
-        mapView.settings.myLocationButton = true
         // Listen to the myLocation property of GMSMapView.
         observation = mapView.observe(\.myLocation, options: [.new]) { [weak self] mapView, _ in
             self?.firstUserLocation = mapView.myLocation
         }
+        locationManager.startUpdatingLocation()
+        mapView.isMyLocationEnabled = true
+        mapView.settings.myLocationButton = true
+        print("Size: \(recordInfoView.intrinsicContentSize.height)")
+        mapView.padding = UIEdgeInsets(
+            top: view.safeAreaInsets.top,
+            left: 0,
+            bottom: recordInfoViewHeightConstraint.constant,
+            right: 0)
     }
     
     deinit {
         observation?.invalidate()
+    }
+        
+    private func addCoordinateToPath(coordinate: CLLocationCoordinate2D) {
+        path.add(coordinate)
+        let polyline = GMSPolyline(path: path)
+        polyline.map = mapView
+    }
+    
+    private func startRouteRecording() {
+        recordButton.setTitle("Stop recording", for: .normal)
+        locationManager.startUpdatingLocation()
+    }
+    
+    private func stopRouteRecording() {
+        recordButton.setTitle("Start recording", for: .normal)
+        locationManager.stopUpdatingLocation()
+    }
+    
+    // MARK: - IBActions
+    
+    @IBAction func recordButtonPressed(_ sender: UIButton) {
+        isRecording = !isRecording
+        isRecording ? startRouteRecording() : stopRouteRecording()
+    }
+    
+    @IBAction func saveButtonPressed(_ sender: UIButton) {
     }
 }
 
@@ -65,12 +102,12 @@ extension MapViewController: GMSMapViewDelegate {
 
 extension MapViewController: CLLocationManagerDelegate {
     func locationManager(_ manager: CLLocationManager, didChangeAuthorization status: CLAuthorizationStatus) {
-      guard status == .authorizedWhenInUse else { return }
+        guard status == .authorizedWhenInUse else { return }
     }
     
     func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
-        
-        print("did update locations")
+        guard let location = locations.first else { return }
+        addCoordinateToPath(coordinate: location.coordinate)
     }
     
     func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
